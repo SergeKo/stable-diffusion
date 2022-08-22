2b00ae19a98d8cb82d71f64bc54a322edfaa1060
Stress Dev <kozlovski.serge@gmail.com>
2022-08-23 00:21:51 +0200

    ff

--- main.py
+++ main.py
@@ -24,6 +24,8 @@ from torch import autocast
 from diffusers import StableDiffusionPipeline, LMSDiscreteScheduler
 
 model_id = "CompVis/stable-diffusion-v1-4"
+device = "cuda"
+
 # Use the K-LMS scheduler here instead
 scheduler = LMSDiscreteScheduler(beta_start=0.00085, beta_end=0.012, beta_schedule="scaled_linear", num_train_timesteps=1000)
 pipe = StableDiffusionPipeline.from_pretrained(model_id, scheduler=scheduler, use_auth_token=True)
